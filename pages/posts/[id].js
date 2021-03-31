69b42fe2a9980ac4ad225a86314db1f53d1b09cc
Ivaylo <ivaylopehlivanov@abv.bg>
2021-03-31 17:25:27 +0300

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import Date from "../../components/Date";
+import Layout from "../../components/Layout";
+import { getAllPostsIds, getPostData } from "../../lib/posts";
+import utilStyles from '../../styles/utils.module.css';
+
+export const getStaticPaths = async () => {
+    const paths = getAllPostsIds();
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async ({ params }) => {
+    const postData = await getPostData(params.id);
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
+const Post = ({ postData }) => {
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
+    )
+}
+
+export default Post;
